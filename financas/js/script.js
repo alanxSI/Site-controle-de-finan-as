0b1c12aa2207f1648cbd0f4c77115ed327cd4ff9
alan <alandesouzadonascimento@gmail.com>
2026-08-01 17:18:40 -0300

    adicionei as paginas de ver detalhes em categorias e adicionar dinheiro em metas. tambem adicionei o js pra funcionalidade de tais botoes

--- financas/js/script.js
+++ financas/js/script.js
@@ -103,3 +103,131 @@ botaovoltarmeta.onclick = function() {
     metas.style.display = 'block';
     barranavegacao.style.display = 'block flex';
 }
+
+/*-------------VER DETALHES CATEGORIAS---------------------*/
+
+var botaodetalhesalimentacao = document.getElementById('botaodetalhesalimentacao');
+var detalhesalimentacao = document.getElementById('detalhesalimentacao');
+var botaovoltardetalhesalimentacao = document.getElementById('botaovoltardetalhesalimentacao');
+
+botaodetalhesalimentacao.onclick = function() {
+    categorias.style.display = 'none';
+    detalhesalimentacao.style.display = 'block';
+    barranavegacao.style.display = 'none';
+}
+
+botaovoltardetalhesalimentacao.onclick = function() {
+    detalhesalimentacao.style.display = 'none';
+    categorias.style.display = 'block';
+    barranavegacao.style.display = 'block flex';
+}
+
+var botaodetalhestransporte = document.getElementById('botaodetalhestransporte');
+var detalhestransporte = document.getElementById('detalhestransporte');
+var botaovoltardetalhestransporte = document.getElementById('botaovoltardetalhestransporte');
+
+botaodetalhestransporte.onclick = function() {
+    categorias.style.display = 'none';
+    detalhestransporte.style.display = 'block';
+    barranavegacao.style.display = 'none';
+}
+
+botaovoltardetalhestransporte.onclick = function() {
+    detalhestransporte.style.display = 'none';
+    categorias.style.display = 'block';
+    barranavegacao.style.display = 'block flex';
+}
+
+var botaodetalheslazer = document.getElementById('botaodetalheslazer');
+var detalheslazer = document.getElementById('detalheslazer');
+var botaovoltardetalheslazer = document.getElementById('botaovoltardetalheslazer'); 
+
+botaodetalheslazer.onclick = function() {
+    categorias.style.display = 'none';
+    detalheslazer.style.display = 'block';
+    barranavegacao.style.display = 'none';
+}
+
+botaovoltardetalheslazer.onclick = function() {
+    detalheslazer.style.display = 'none';
+    categorias.style.display = 'block';
+    barranavegacao.style.display = 'block flex';
+}  
+
+var botaodetalhessaude = document.getElementById('botaodetalhessaude');
+var detalhessaude = document.getElementById('detalhessaude');
+var botaovoltardetalhessaude = document.getElementById('botaovoltardetalhessaude');
+
+botaodetalhessaude.onclick = function() {
+    categorias.style.display = 'none';
+    detalhessaude.style.display = 'block';
+    barranavegacao.style.display = 'none';
+}
+
+botaovoltardetalhessaude.onclick = function() {
+    detalhessaude.style.display = 'none';
+    categorias.style.display = 'block';
+    barranavegacao.style.display = 'block flex';
+}
+
+var botaodetalhesestudos = document.getElementById('botaodetalhesestudos');
+var detalhesestudos = document.getElementById('detalhesestudos');
+var botaovoltardetalhesestudos = document.getElementById('botaovoltardetalhesestudos');
+
+botaodetalhesestudos.onclick = function() {
+    categorias.style.display = 'none';
+    detalhesestudos.style.display = 'block';
+    barranavegacao.style.display = 'none';
+}
+
+botaovoltardetalhesestudos.onclick = function() {
+    detalhesestudos.style.display = 'none';
+    categorias.style.display = 'block';
+    barranavegacao.style.display = 'block flex';
+}
+
+var botaodetalhesselfcare = document.getElementById('botaodetalhesselfcare');
+var detalhesselfcare = document.getElementById('detalhesselfcare');
+var botaovoltardetalhesselfcare = document.getElementById('botaovoltardetalhesselfcare');
+
+botaodetalhesselfcare.onclick = function() {
+    categorias.style.display = 'none';
+    detalhesselfcare.style.display = 'block';
+    barranavegacao.style.display = 'none';
+}
+
+botaovoltardetalhesselfcare.onclick = function() {
+    detalhesselfcare.style.display = 'none';
+    categorias.style.display = 'block';
+    barranavegacao.style.display = 'block flex';
+}
+
+/*-----------------ADICIONAR DINHEIRO NA META--------------------*/
+var botaoadicionardinheironotebook = document.getElementById('botaoadicionardinheironotebook');
+var botaoadicionardinheiromoto = document.getElementById('botaoadicionardinheiromoto');
+var botaovoltardinheironotebook = document.getElementById('botaovoltardinheironotebook');
+var botaovoltardinheiromoto = document.getElementById('botaovoltardinheiromoto');
+
+botaoadicionardinheironotebook.onclick = function() {
+    metas.style.display = 'none';
+    adicionardinheironotebook.style.display = 'block';
+    barranavegacao.style.display = 'none';
+}
+
+botaoadicionardinheiromoto.onclick = function() {
+    metas.style.display = 'none';
+    adicionardinheiromoto.style.display = 'block';
+    barranavegacao.style.display = 'none';
+}
+
+botaovoltardinheiromoto.onclick = function() {
+    adicionardinheiromoto.style.display = 'none';
+    metas.style.display = 'block';
+    barranavegacao.style.display = 'block flex';
+}
+
+botaovoltardinheironotebook.onclick = function() {
+    adicionardinheironotebook.style.display = 'none';
+    metas.style.display = 'block';
+    barranavegacao.style.display = 'block flex';
+}
